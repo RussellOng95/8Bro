6833c0f4d345924e475691da2de877536779d859
russell <russell>
2019-05-22 22:25:14 +0800

    Initial Commit

---
+++ 8Bro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8Bro
+//
+//  Created by Russell Ong on 22/5/19.
+//  Copyright © 2019 Russell Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
